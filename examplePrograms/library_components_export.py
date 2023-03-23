0f308c6c80d9081ffa160fa3a7be6bd6d5b3703d
Rowan Murray <67557980+RowMur@users.noreply.github.com>
2023-03-23 16:22:52 +0000

    Minor fixes after review, switch to DictWriter

--- examplePrograms/library_components_export.py
+++ examplePrograms/library_components_export.py
@@ -3,11 +3,11 @@ import os
 import json
 import csv
 
+workspace_url = "Your workspace URL here"
+
 # This is the API query, feel free to chop and change this to suit your use case.
 # We recommend building queries in an IDE such as BananaCakePop or Postman.
 # You can use BananaCakePop in a browser here: https://api.nexar.com/graphql/
-
-workspaceURL = "Your workspace URL here"
 
 graphql_query = '''query library ($workspaceURL: String!, $after: String) {
   desLibrary (workspaceUrl: $workspaceURL) {
@@ -56,14 +56,11 @@ if __name__ == "__main__":
     nexar = NexarClient(client_id, client_secret, ["design.domain", "openid", "user.access"])
 
     def retrieve_components():
-
         page_info = {"hasNextPage": True, "endCursor": None}
         components = []
-
-        while page_info["hasNextPage"] is True:
-
+        while page_info["hasNextPage"]:
             variables = {
-                "workspaceURL": workspaceURL,
+                "workspaceURL": workspace_url,
                 "after": page_info["endCursor"]
             }
             response = nexar.get_query(graphql_query, variables)
@@ -75,51 +72,60 @@ if __name__ == "__main__":
         return components
 
     def json_export(components):
-
         with open("altium_library_components.json", "w") as json_file:
-
             json.dump(components, json_file)
 
     def csv_export(components):
-
         with open("altium_library_components.csv", "w", newline="") as csv_file:
-
-            writer = csv.writer(csv_file)
+            writer = csv.DictWriter(csv_file, fieldnames=[
+                # These are the headers for the file
+                "Item ID",
+                "Name",
+                "Type",
+                "Description",
+                "RoHS Compliant",
+                "Manufacturer 1",
+                "Partnumber 1",
+                "Manufacturer 2",
+                "Partnumber 2",
+                "Manufacturer 3",
+                "Partnumber 3",
+            ])
 
             # This is just an example of how you might want to format your data in a csv file.
             # Feel free to edit this to format it to your liking.
 
-            writer.writerow([
-                # These are the headers for the file
-                "Item ID", "Name", "Type", "Description",
-                "RoHS Compliant",
-                "Manufacturer 1", "Partnumber 1",
-                "Manufacturer 2", "Partnumber 2",
-                "Manufacturer 3", "Partnumber 3"
-            ])
-            writer.writerow([])
-
+            writer.writeheader()
             for component in components:
 
                 # This is an example of how to extract a specific parameter for each component.
-                # Feel free to modify this to get the parameter you wish to.
-
                 rohs_compliant_value = ""
                 for parameter in component["details"]["parameters"]:
+                    # Feel free to modify this to get the parameter you wish to.
+                    # You will also need to modify the header, row and variable names accordingly
                     if parameter["name"] == "RoHS Compliant":
                         rohs_compliant_value = parameter["value"]
 
-                row = [
-                    component["name"],
-                    component["comment"],
-                    component["folder"]["name"],
-                    component["description"],
-                    rohs_compliant_value,
-                ]
+                row = {
+                    "Item ID": component["name"],
+                    "Name": component["comment"],
+                    "Type": component["folder"]["name"],
+                    "Description": component["description"],
+                    "RoHS Compliant": rohs_compliant_value,
+                }
 
-                for manufacturerPart in component["manufacturerParts"]:
-                    row.append(manufacturerPart["companyName"])
-                    row.append(manufacturerPart["partNumber"])
+                # In this example we are slicing the first 3 manufacturer parts
+                # Feel free to not by removing the slice at the end
+                # You will also need to add the appropriate column headers above
+                for manufacturer_part in component["manufacturerParts"][:3]:
+                    row.update({
+                        "Manufacturer " + str(component["manufacturerParts"].index(manufacturer_part)+1):
+                        manufacturer_part["companyName"]
+                    })
+                    row.update({
+                        "Partnumber " + str(component["manufacturerParts"].index(manufacturer_part)+1):
+                        manufacturer_part["partNumber"]
+                    })
 
                 writer.writerow(row)
 
