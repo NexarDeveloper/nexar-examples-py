1cb2aa2106251499d78c8790f0d344fb9f9d2ca8
Rowan Murray <67557980+RowMur@users.noreply.github.com>
2023-03-23 16:33:46 +0000

    Add ability to create and delete comment threads

--- examplePrograms/project_change_comments.py
+++ examplePrograms/project_change_comments.py
@@ -11,8 +11,10 @@ from nexarClients.design.design_helpers import (
 UPDATE_ACTION = "Update comment"
 CREATE_ACTION = "Create comment"
 DELETE_ACTION = "Delete comment"
+CREATE_THREAD_ACTION = "Create comment thread"
+DELETE_THREAD_ACTION = "Delete comment thread"
 
-COMMENT_ACTIONS = [UPDATE_ACTION, CREATE_ACTION, DELETE_ACTION]
+COMMENT_ACTIONS = [UPDATE_ACTION, CREATE_ACTION, DELETE_ACTION, CREATE_THREAD_ACTION, DELETE_THREAD_ACTION]
 
 PROJECTWITHCOMMENTS_QUERY = '''
 query desProjectById($projectId : ID!) {
@@ -23,6 +25,8 @@ query desProjectById($projectId : ID!) {
             workInProgress{
                 variants{
                     pcb{
+                        documentId
+                        documentName
                         commentThreads{
                             threadNumber
                             commentThreadId
@@ -87,24 +91,51 @@ mutation desDeleteComment($entityId : ID!, $commentThreadId : String!, $commentI
 }
 '''
 
+CREATECOMMENTTHREAD_MUTATION = '''
+mutation createCommentThread ($entityId: ID!, $documentId: String!, $text: String!){
+    desCreateCommentThread (input: {
+        documentType: PCB,
+        commentContextType: NONE,
+        entityId: $entityId,
+        documentId: $documentId,
+        text: $text,
+    }) {
+        commentThreadId
+        commentId
+        errors {
+            message
+        }
+    }
+}
+'''
 
-def get_comment_threads_from_project(project_id, nexar):
+DELETECOMMENTTHREAD_MUTATION = '''
+mutation deleteCommentThread ($entityId: ID!, $commentThreadId: String!) {
+    desDeleteCommentThread (input: {
+        entityId: $entityId,
+        commentThreadId: $commentThreadId
+    }) {
+        errors {
+            message
+        }
+    }
+}
+'''
+
+
+def get_pcb_from_project(project_id, nexar):
     variables = {
         "projectId": project_id
     }
-    commentThreads = nexar.get_query(
+    pcb = nexar.get_query(
         PROJECTWITHCOMMENTS_QUERY, variables
-    )["desProjectById"]["design"]["workInProgress"]["variants"][0]["pcb"]["commentThreads"]
-    return commentThreads
+    )["desProjectById"]["design"]["workInProgress"]["variants"][0]["pcb"]
+    return pcb
 
 
-def list_comment_threads(commentThreads):
+def list_comment_threads(pcb):
+    commentThreads = pcb["commentThreads"]
     print()
-
-    if commentThreads == []:
-        print("No comments available, exiting...")
-        print()
-        sys.exit()
 
     for thread in commentThreads:
         print("Comment thread :", thread["threadNumber"])
@@ -123,30 +154,61 @@ def list_comment_threads(commentThreads):
             print()
 
 
-def get_user_decision(commentThreads):
-    if commentThreads == []:                                    # if there are no comments
+def get_user_decision(pcb):
+    if pcb["commentThreads"] == []:                                    # if there are no comments
         print("No comment to update, or thread to add to...")
-        sys.exit()
+
+        while True:
+            new_thread_decision = input("Would you like to create a comment thread? (Y/N): ")
+
+            if new_thread_decision.lower() == "n":
+                sys.exit()
+            elif new_thread_decision.lower() == "y":
+                return CREATE_THREAD_ACTION
+            else:
+                print("Invalid response.\n")
+                continue
 
     for index, item in enumerate(COMMENT_ACTIONS):
         print(index + 1, ":", item)
 
-    print()
-    choice = input("Enter number to update, create or delete a comment... : ")
-    choice = int(choice)
-    print()
+    while True:
 
-    if choice <= 0 or choice > len(COMMENT_ACTIONS):
-        print("Invalid response, try again... " + "\n")
-        return get_user_decision(commentThreads)
+        print()
+        choice = input("Enter number to update, create, delete a comment or create a new comment thread: ")
+        choice = int(choice)
+        print()
 
-    return COMMENT_ACTIONS[choice-1]
+        if choice <= 0 or choice > len(COMMENT_ACTIONS):
+            print("Invalid response, try again... " + "\n")
+            continue
+
+        return COMMENT_ACTIONS[choice-1]
 
 
-def change_comment(commentThreads, choice, entityId):
+def change_comment(pcb, choice, entityId):
     if choice == CREATE_ACTION:
         commentThreadId = input("Enter comment thread ID for the section you want to add a comment to... : ")
         create_comment(nexar, entityId, commentThreadId)
+
+    elif choice == CREATE_THREAD_ACTION:
+        create_comment_thread(nexar, entityId, pcb["documentId"])
+
+    elif choice == DELETE_THREAD_ACTION:
+        while True:
+
+            commentThreadId = input("Enter a comment thread ID for the thread you want to delete... : ")
+            valid_comment_thread_ids = []
+
+            for commentThread in pcb["commentThreads"]:
+                valid_comment_thread_ids.append(commentThread["commentThreadId"])
+
+            if commentThreadId in valid_comment_thread_ids:
+                break
+            else:
+                continue
+
+        delete_comment_thread(nexar, entityId, commentThreadId)
 
     else:
         if choice == UPDATE_ACTION:
@@ -158,9 +220,10 @@ def change_comment(commentThreads, choice, entityId):
         commentId = input(currentText)
         commentThreadId = ""
 
-        for commentThread in commentThreads:
+        for commentThread in pcb["commentThreads"]:
 
-            for comments in commentThread["comments"]:                     # gets commentThreadId from commentId
+            for comments in commentThread["comments"]:
+                # gets commentThreadId from commentId
 
                 if comments["commentId"] == commentId:
                     commentThreadId = commentThread["commentThreadId"]
@@ -168,13 +231,49 @@ def change_comment(commentThreads, choice, entityId):
         # if the for loop could not find a commentThreadId, meaning the comment ID used was invalid
         if commentThreadId == "":
             print("\n" + "Invalid Comment ID..." + "\n")
-            change_comment(commentThreads, choice, entityId)                   # loops until correct comment ID is used
+            change_comment(pcb, choice, entityId)
+            # loops until correct comment ID is used
 
         if choice == UPDATE_ACTION:
             update_comment(nexar, entityId, commentThreadId, commentId)
 
         else:
             delete_comment(nexar, entityId, commentThreadId, commentId)
+
+
+def create_comment_thread(nexar, entityId, documentId):
+    print()
+    text = input("Enter comment: ")
+    variables = {
+        "entityId": entityId,
+        "documentId": documentId,
+        "text": text,
+    }
+
+    create_thread_response = nexar.get_query(CREATECOMMENTTHREAD_MUTATION, variables)
+
+    errors = create_thread_response["desCreateCommentThread"]["errors"]
+    if errors != []:
+        print(errors)
+
+    else:
+        print("Comment thread successfully created.\n")
+
+
+def delete_comment_thread(nexar, entityId, commentThreadId):
+    print()
+    variables = {
+        "entityId": entityId,
+        "commentThreadId": commentThreadId
+    }
+
+    delete_thread_response = nexar.get_query(DELETECOMMENTTHREAD_MUTATION, variables)
+
+    errors = delete_thread_response["desDeleteCommentThread"]["errors"]
+    if errors != []:
+        print(errors)
+    else:
+        print("Comment thread successfully deleted.\n")
 
 
 def update_comment(nexar, entityId, commentThreadId, commentId):
@@ -245,8 +344,8 @@ if __name__ == '__main__':
     projects = get_projects(workspace_id, nexar)
     project_id = get_project_id_from_user(projects)
 
-    commentThreads = get_comment_threads_from_project(project_id, nexar)
-    list_comment_threads(commentThreads)
+    pcb = get_pcb_from_project(project_id, nexar)
+    list_comment_threads(pcb)
 
-    choice = get_user_decision(commentThreads)
-    change_comment(commentThreads, choice, project_id)
+    choice = get_user_decision(pcb)
+    change_comment(pcb, choice, project_id)
